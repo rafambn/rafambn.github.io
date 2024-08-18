06647b3dadeb445d894d9cebe4d377f845d49b2c
Rafael Mendonça <rafa_mbn@hotmail.com>
2024-08-17 23:19:05 -0300

    Added my description div; Added href for my info; Improve placement;

--- theme.js
+++ theme.js
@@ -2,12 +2,12 @@ const $html = document.querySelector('html');
 
 function enableDarkMode() {
     $html.classList.toggle('dark-mode')
-    document.getElementById('sun').style.right = '-100px';
-    document.getElementById('moon').style.right = '50px';
+    document.getElementById('sun').style.right = '-10vh';
+    document.getElementById('moon').style.right = '2.5vh';
 }
 
 function disableDarkMode() {
     $html.classList.toggle('dark-mode')
-    document.getElementById('sun').style.right = '50px';
-    document.getElementById('moon').style.right = '-100px';
+    document.getElementById('sun').style.right = '2.5vh';
+    document.getElementById('moon').style.right = '-10vh';
 }
