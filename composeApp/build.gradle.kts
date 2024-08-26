5196cfbb2ebba7548d9d3cb60863a9d1dfac0e6a
Rafael Mendonça <rafa_mbn@hotmail.com>
2024-08-26 18:37:55 -0300

    Clear project

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,7 +20,6 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation(compose.preview)
         }
     }
 }
