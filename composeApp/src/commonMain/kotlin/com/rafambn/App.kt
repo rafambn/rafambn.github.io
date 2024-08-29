2c5adfa86ad28e7c00b824d0f286fdbaa975b8fb
Rafael Mendonça <rafa_mbn@hotmail.com>
2024-08-29 10:35:36 -0300

    Placement now is responsive

--- composeApp/src/commonMain/kotlin/com/rafambn/App.kt
+++ composeApp/src/commonMain/kotlin/com/rafambn/App.kt
@@ -11,12 +11,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.toIntSize
 import com.rafambn.theme.PageTheme
 import my_web_page.composeapp.generated.resources.Res
 import my_web_page.composeapp.generated.resources.airplane_banner
@@ -26,14 +24,10 @@ import my_web_page.composeapp.generated.resources.github_mark
 import my_web_page.composeapp.generated.resources.linkedln_mark
 import my_web_page.composeapp.generated.resources.plane
 import org.jetbrains.compose.resources.imageResource
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.vectorResource
 
 @Composable
 fun App(windowSizeDp: DpSize) = PageTheme {
-    val windowSizePx = with(LocalDensity.current) {
-        windowSizeDp.toSize().toIntSize()
-    }
     Box(
         Modifier.fillMaxSize().background(color = Color(66, 135, 245))
     ) {
@@ -43,31 +37,36 @@ fun App(windowSizeDp: DpSize) = PageTheme {
             modifier = Modifier.align(BiasAlignment(0f, -0.8f))
                 .size(windowSizeDp.height * 3 / 10)
         )
-        val halfWidth = (windowSizePx / 2).width
-        val halfProfile = windowSizePx.height * 3 / 20
-        val balloonWidth = windowSizePx.height * 68 / 1000
-        val profileSizeSpace = (windowSizePx.width - halfWidth - halfProfile - balloonWidth)
-        val gitPlacement = profileSizeSpace / 2 + halfWidth + halfProfile
-        val linkedinPlacement = profileSizeSpace / 3
-        val emailPlacement = profileSizeSpace * 2 / 3
         Balloon(
             vectorResource(Res.drawable.github_mark),
             "https://github.com/rafambn",
             windowSizeDp.height * 1 / 10,
-            Modifier.align(OffsetAlignment(IntOffset(gitPlacement, windowSizePx.height * 5 / 20)))
+            Modifier.align(
+                OffsetAlignment { _, space, _ ->
+                    IntOffset(
+                        (space.width / 2 - space.height * 3 / 20 - space.height * 68 / 1000) / 2 + space.width / 2 + space.height * 3 / 20,
+                        space.height * 5 / 20)
+                }
+            )
         )
         Balloon(
             vectorResource(Res.drawable.linkedln_mark),
             "https://www.linkedin.com/in/rafambn/",
             windowSizeDp.height * 1 / 10,
-            Modifier.align(OffsetAlignment(IntOffset(linkedinPlacement, windowSizePx.height / 10)))
+            Modifier.align(
+                OffsetAlignment { _, space, _ ->
+                    IntOffset((space.width / 2 - space.height * 3 / 20 - space.height * 68 / 1000) / 3, space.height / 10)
+                }
+            )
         )
         Balloon(
             vectorResource(Res.drawable.email_mark),
             "mailto:rafambn@gmail.com",
             windowSizeDp.height * 1 / 10,
             Modifier.align(
-                OffsetAlignment(IntOffset(emailPlacement, windowSizePx.height * 3 / 10))
+                OffsetAlignment { _, space, _ ->
+                    IntOffset((space.width / 2 - space.height * 3 / 20 - space.height * 68 / 1000) * 2 / 3, space.height * 3 / 10)
+                }
             )
         )
         Image(
@@ -75,25 +74,29 @@ fun App(windowSizeDp: DpSize) = PageTheme {
             contentDescription = "balloon",
             modifier = Modifier.align(BiasAlignment(0F, -0.2F)).size(windowSizeDp.height * 7 / 20)
         )
-        val halfBannerWidth = ((windowSizePx.height * 7 / 20) / 2)
-        val ropeAttachWidth = (windowSizePx.height * 1 / 100)
         Image(
             imageVector = vectorResource(Res.drawable.plane),
             contentDescription = "balloon",
-            modifier = Modifier.align(OffsetAlignment(IntOffset(halfWidth + halfBannerWidth - ropeAttachWidth, windowSizePx.height * 39 / 100))).size(windowSizeDp.height * 1 / 10)
+            modifier = Modifier
+                .align(
+                    OffsetAlignment { _, space, _ ->
+                        IntOffset(space.width / 2 + (space.height * 7 / 20) / 2 - (space.height * 1 / 100), space.height * 39 / 100)
+                    }
+                )
+                .size(windowSizeDp.height * 1 / 10)
         )
     }
 }
 
 @Immutable
 data class OffsetAlignment(
-    val intOffset: IntOffset
+    val lambda: (size: IntSize, space: IntSize, layoutDirection: LayoutDirection) -> IntOffset
 ) : Alignment {
     override fun align(
         size: IntSize,
         space: IntSize,
         layoutDirection: LayoutDirection
     ): IntOffset {
-        return intOffset
+        return lambda(size, space, layoutDirection)
     }
 }
